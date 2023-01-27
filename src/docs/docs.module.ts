79d5aab67f73edcaeb970c0d6d3e6f3b358adfcc
-aggrawal <.m@northeastern.edu>
2023-01-27 16:03:24 +0000

    feat(docs): init module & swagger package

--- src/docs/docs.module.ts
+++ src/docs/docs.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { DocsService } from './docs.service';
 
 @Module({
-  providers: [DocsService]
+	providers: [DocsService]
 })
 export class DocsModule {}
