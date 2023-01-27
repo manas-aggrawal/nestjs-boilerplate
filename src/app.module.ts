4493682019ed9ef2c5605957850fca560675dc23
-aggrawal <.m@northeastern.edu>
2023-01-27 16:03:06 +0000

    feat(docs): init module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DocsModule } from './docs/docs.module';
 
 @Module({
-	imports: [AuthModule],
+	imports: [AuthModule, DocsModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
