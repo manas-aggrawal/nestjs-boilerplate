33584600af8c44fceb0ab17b8d3e42c154ce1492
-aggrawal <.m@northeastern.edu>
2023-02-06 11:26:39 +0000

    feat(auth): add service & validate user service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async validateUser(usernameField: string, password: string) {
+		const user = await this.prisma.user.findFirst({
+			where: {
+				username: usernameField,
+			}
+		});
+		if(user) {
+			const validatePassword = await bcrypt.compare(password, user.password);
+			if(validatePassword) {
+				return user;
+			}
+		}
+		throw new UnauthorizedException({
+			message: 'username or password are invalid'
+		});
+	}
+}
