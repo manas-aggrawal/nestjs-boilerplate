ccfdcfe415e9f9c6055aaea03139d2448a41f5bd
-aggrawal <.m@northeastern.edu>
2023-02-07 10:48:26 +0000

    feat: add access token guard in the whole app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
+import { AccessTokenGuard } from './auth/guards/accessToken.guard';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
 	imports: [AuthModule, PrismaModule],
 	controllers: [AppController],
-	providers: [AppService, PrismaService],
+	providers: [
+		AppService,
+		PrismaService,
+		{
+			provide: APP_GUARD,
+			useClass: AccessTokenGuard,
+		},
+	],
 })
 export class AppModule {}
