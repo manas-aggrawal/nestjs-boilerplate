05c9616c1646b213cc3877e38a9bbec793a050e9
-aggrawal <.m@northeastern.edu>
2023-02-07 10:49:17 +0000

    feat: add strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Global, Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
+import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 
 @Global()
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService, LocalStrategy]
+	providers: [
+		AuthService,
+		LocalStrategy,
+		AccessTokenStrategy,
+		RefreshTokenStrategy,
+		JwtService,
+	],
 })
 export class AuthModule {}
