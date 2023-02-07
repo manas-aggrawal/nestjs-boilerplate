83e3abf4b678b93332de45210e409f7036d67b65
-aggrawal <.m@northeastern.edu>
2023-02-07 10:55:51 +0000

    feat: local strategy

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -4,12 +4,15 @@ import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy, 'local-strategy') {
+export class LocalStrategy extends PassportStrategy(
+	Strategy,
+	'local-strategy',
+) {
 	constructor(private readonly authService: AuthService) {
 		super(/*{usernameField: 'email'}*/); // default usernameField is username
 	}
 
-	validate(usernameField: string, password: string) {
-		return this.authService.validateUser(usernameField, password);
+	async validate(usernameField: string, password: string) {
+		return await this.authService.validateUser(usernameField, password);
 	}
 }
