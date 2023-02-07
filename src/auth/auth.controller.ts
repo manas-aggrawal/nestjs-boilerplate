e11fcca49d9497baf3eac07b75ccc2ed13b22292
-aggrawal <.m@northeastern.edu>
2023-02-07 10:48:58 +0000

    feat: add guards & refresh-token route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/isPublic';
 import { LocalGuard } from './guards/local.guard';
+import { RefreshTokenGuard } from './guards/refreshToken.guard';
 
 @Controller()
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
+	@IsPublic()
 	@Post('/login')
 	@UseGuards(LocalGuard)
-	async login() {
-		return 'return access token & refresh token';
+	async login(@Request() req) {
+		return this.authService.giveTokens(req.user);
+	}
+
+	@IsPublic()
+	@UseGuards(RefreshTokenGuard)
+	@Get('/refresh-token')
+	async refreshToken(@Request() req) {
+		return this.authService.giveTokens(req.user);
 	}
 }
