a3195b16922b76489d5877547ecd708913637f61
-aggrawal <.m@northeastern.edu>
2023-02-28 16:16:09 +0000

    feat: add user module & add forgot password token strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,23 @@
 import { Global, Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
+import { ForgotPasswordTokenStrategy } from './strategies/forgot-password-token.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
 
 @Global()
 @Module({
+	imports: [UserModule],
 	controllers: [AuthController],
 	providers: [
 		AuthService,
 		LocalStrategy,
 		AccessTokenStrategy,
 		RefreshTokenStrategy,
+		ForgotPasswordTokenStrategy,
 		JwtService,
 	],
 })
