5f59b3a7ee7bda95df3500ac72625cfada372106
-aggrawal <.m@northeastern.edu>
2023-02-28 14:44:23 +0000

    chore: put file name into pattern

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AccessTokenStrategy } from './strategies/accessToken.strategy';
+import { AccessTokenStrategy } from './strategies/access-token.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
 
 @Global()
 @Module({
