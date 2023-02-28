d06e81664de6eea6ce764b162f6ff1c92ef22372
-aggrawal <.m@northeastern.edu>
2023-02-28 16:16:30 +0000

    feat: add forgot and update password methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { ForgotPasswordPayload } from './dto/forgot-password-payload.dto';
 import { DefaultPayload } from './interfaces/payload.interface';
 
 @Injectable()
@@ -11,6 +13,7 @@ export class AuthService {
 	constructor(
 		private readonly prisma: PrismaService,
 		private readonly jwtService: JwtService,
+		private readonly userService: UserService,
 	) {}
 
 	async validateUser(usernameField: string, password: string) {
@@ -45,5 +48,25 @@ export class AuthService {
 			access_token,
 			refresh_token,
 		};
+	}
+
+	async forgotPassword(forgotPasswordPayload: ForgotPasswordPayload) {
+		const { username } = forgotPasswordPayload;
+		const user = await this.prisma.user.findFirst({
+			where: {
+				username,
+			},
+		});
+		if (user) {
+			// Send an email to user and return a jwt token with the secret password
+			return 'An email has been send to reset password';
+		}
+		throw new UnauthorizedException({
+			message: 'username is invalid',
+		});
+	}
+
+	async updatePassword(password: string, userId: string) {
+		return await this.userService.updateUserPassword(userId, password);
 	}
 }
