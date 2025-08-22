a03ee1836b58edfd5804706c35219ba8a84eedaa
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2025-08-22 09:00:00 -0400

    feat(common): add logger config, validator guard, and middleware

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+	private logger = new Logger('HTTP');
+
+	use(req: Request, res: Response, next: (error?: unknown) => void) {
+		this.logger.log(
+			`Logging HTTP request ${req.method} | ${req.originalUrl} | ${res.statusCode}`,
+		);
+		next();
+	}
+}
